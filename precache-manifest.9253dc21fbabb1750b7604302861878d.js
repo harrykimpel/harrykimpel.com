6aaf6f67094a04e0e091a697dcf0ca429d12ee3c
Harry Kimpel <hkimpel@newrelic.com>
2020-11-18 19:28:34 +0100

    teams 2

---
+++ precache-manifest.9253dc21fbabb1750b7604302861878d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714c082e766e3c08c6591ed3d6e9eef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3339e0cf154dcfdada41",
+    "url": "/static/css/main.4a1b6a88.chunk.css"
+  },
+  {
+    "revision": "1e7576ddb9de037695a4",
+    "url": "/static/js/2.1fee81b5.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/static/js/2.1fee81b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3339e0cf154dcfdada41",
+    "url": "/static/js/main.891b7b63.chunk.js"
+  },
+  {
+    "revision": "9bfd6d225e4168839554",
+    "url": "/static/js/runtime-main.22145939.js"
+  }
+]);
