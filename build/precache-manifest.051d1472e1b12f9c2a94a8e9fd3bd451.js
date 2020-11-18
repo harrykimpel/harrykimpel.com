d8a502f3e8a950031089ff9583d01a86b418602c
Harry Kimpel <hkimpel@newrelic.com>
2020-11-18 18:52:59 +0100

    teams-app

---
+++ build/precache-manifest.051d1472e1b12f9c2a94a8e9fd3bd451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25fa633520ea3228ec34a0d13d94ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7294784b3be6f1fb9b1f",
+    "url": "/static/css/main.4a1b6a88.chunk.css"
+  },
+  {
+    "revision": "1e7576ddb9de037695a4",
+    "url": "/static/js/2.1fee81b5.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/static/js/2.1fee81b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7294784b3be6f1fb9b1f",
+    "url": "/static/js/main.c16b9fd9.chunk.js"
+  },
+  {
+    "revision": "9bfd6d225e4168839554",
+    "url": "/static/js/runtime-main.22145939.js"
+  }
+]);
