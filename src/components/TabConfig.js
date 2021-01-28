1c5c9f3d8f21d3745e3bdd3c6832d555eae60197
Harry Kimpel <harrykimpel@hotmail.com>
2021-01-28 09:16:12 +0100

    Update TabConfig.js

--- src/components/TabConfig.js
+++ src/components/TabConfig.js
@@ -19,7 +19,7 @@ class TabConfig extends React.Component {
      * This allows for the addition of query string parameters based on
      * the settings selected by the user.
      */
-    microsoftTeams.settings.setSettings({ "contentUrl": "https://calm-flower-0ef275b10.azurestaticapps.net/build" });
+    microsoftTeams.settings.setSettings({ "contentUrl": "https://calm-flower-0ef275b10.azurestaticapps.net/" });
 
     /**
      * After verifying that the settings for your tab are correctly
